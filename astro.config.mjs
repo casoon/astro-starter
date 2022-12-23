6e174135639d2a4f564ee249dafddde8106a30e4
casoon <joern.seidel@casoon.de>
2022-12-23 13:00:04 +0100

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,24 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig(
+    {
+        server: { port: 8080 },
+        site: 'https://www.changethis.com',
+        integrations: [
+            sitemap({
+                changefreq: 'weekly',
+                priority: 0.7,
+                lastmod: new Date('2022-12-21')}),
+            image({serviceEntryPoint: '@astrojs/image/sharp', cacheDir: "./.cache/image", logLevel: 'debug'}),
+            compress({
+                css: false,
+                html: true,
+                img: false,
+                js: false,
+                svg: false,
+                logger: 0,
+            })
+        ],
+    }
+);
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,5 @@
 module.exports = {
   plugins: {
-    "postcss-import": {},
-    "postcss-media-minmax": {},
-    "postcss-csso": {},
     autoprefixer: {},
     cssnano: {},
     "@fullhuman/postcss-purgecss": {
