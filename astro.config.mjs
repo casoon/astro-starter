152445e299b50d127391237c9936ca3bc0b7b36d
casoon <joern.seidel@casoon.de>
2022-12-23 16:42:57 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig(
